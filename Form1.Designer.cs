ec82a6780f951855e50102cdad30e6db2eaa6b45
Emre Baglama <emrebaglama17@hotmail.com>
2024-09-14 10:37:36 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace MFdeneme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProcessImages = new System.Windows.Forms.Button();
+            this.txt_genislik = new System.Windows.Forms.TextBox();
+            this.txt_yukseklik = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // btnProcessImages
+            // 
+            this.btnProcessImages.Location = new System.Drawing.Point(4, 119);
+            this.btnProcessImages.Name = "btnProcessImages";
+            this.btnProcessImages.Size = new System.Drawing.Size(231, 23);
+            this.btnProcessImages.TabIndex = 0;
+            this.btnProcessImages.Text = "Klasörü seç ve başla";
+            this.btnProcessImages.UseVisualStyleBackColor = true;
+            this.btnProcessImages.Click += new System.EventHandler(this.btnProcessImages_Click);
+            // 
+            // txt_genislik
+            // 
+            this.txt_genislik.Location = new System.Drawing.Point(94, 57);
+            this.txt_genislik.Name = "txt_genislik";
+            this.txt_genislik.Size = new System.Drawing.Size(100, 20);
+            this.txt_genislik.TabIndex = 1;
+            this.txt_genislik.Text = "500";
+            // 
+            // txt_yukseklik
+            // 
+            this.txt_yukseklik.Location = new System.Drawing.Point(94, 83);
+            this.txt_yukseklik.Name = "txt_yukseklik";
+            this.txt_yukseklik.Size = new System.Drawing.Size(100, 20);
+            this.txt_yukseklik.TabIndex = 2;
+            this.txt_yukseklik.Text = "750";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Genişlik : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Yükseklik :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(12, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(207, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Resim Formatlama";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(79, 154);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(88, 13);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "0 Fotoğraf İşlendi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(239, 176);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_yukseklik);
+            this.Controls.Add(this.txt_genislik);
+            this.Controls.Add(this.btnProcessImages);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MF - Resim Düzeltme ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProcessImages;
+        private System.Windows.Forms.TextBox txt_genislik;
+        private System.Windows.Forms.TextBox txt_yukseklik;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MFdeneme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Görsel boyutlandırma ve beyaz alan ekleme işlemi
+        private Bitmap ResizeImageWithPadding(Image img, int width, int height)
+        {
+            // Beyaz bir arka plan oluştur
+            Bitmap result = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(result))
+            {
+                g.Clear(Color.White);
+
+                // Görselin orijinal boyutlarını al
+                int originalWidth = img.Width;
+                int originalHeight = img.Height;
+
+                // Görsel istenilen boyutlardan büyükse, oranı koruyarak yeniden boyutlandır
+                float ratio = Math.Min((float)width / originalWidth, (float)height / originalHeight);
+                int newWidth = (int)(originalWidth * ratio);
+                int newHeight = (int)(originalHeight * ratio);
+
+                // Görseli ortalamak için konumu hesapla
+                int xPos = (width - newWidth) / 2;
+                int yPos = (height - newHeight) / 2;
+
+                // Görseli beyaz arka plan üzerine çiz
+                g.DrawImage(img, xPos, yPos, newWidth, newHeight);
+            }
+            return result;
+        }
+
+        // Dosyadaki tüm görselleri işle
+        private void ProcessImages(string folderPath, int targetWidth, int targetHeight)
+        {
+            // Dosya yolundaki tüm resim dosyalarını al
+            string[] files = Directory.GetFiles(folderPath, "*.jpg"); // İsteğe göre dosya formatını değiştirebilirsiniz.
+
+            int durum=0;
+            foreach (var file in files)
+            {
+                Image img = Image.FromFile(file);
+                Bitmap resizedImage = ResizeImageWithPadding(img, targetWidth, targetHeight);
+
+                // Sonuç dosyasını kaydet
+                string savePath = Path.Combine(folderPath, Path.GetFileName(file));
+                resizedImage.Save(savePath);
+                img.Dispose();
+                resizedImage.Dispose();
+                durum++;
+            }
+            MessageBox.Show("Tüm resimler başarıyla işlenmiştir.");
+            linkLabel1.Text = durum + " Fotoğraf İşlendi";
+        }
+
+        private void btnProcessImages_Click(object sender, EventArgs e)
+        {
+
+            int gen, yuk;
+            if(!int.TryParse(txt_genislik.Text, out gen))
+            {
+                MessageBox.Show("Genişlik parametresi hatalıdır. Kontrol ediniz.");
+                return;
+            }
+
+            if (!int.TryParse(txt_yukseklik.Text, out yuk))
+            {
+                MessageBox.Show("Yukseklik parametresi hatalıdır. Kontrol ediniz.");
+                return;
+            }
+
+            // Klasör seçme dialogu
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    string folderPath = fbd.SelectedPath;
+                    int targetWidth = gen;  // Hedef genişlik
+                    int targetHeight = yuk; // Hedef yükseklik
+
+                    // Görselleri işleme fonksiyonu çağrılır
+                    ProcessImages(folderPath, targetWidth, targetHeight);
+                }
+            }
+        }
+    }
+}
